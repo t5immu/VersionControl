b8d60ab8600b849e44f4e051941ec4922eccb42a
János Attila Tapolcsányi <janos.tapolcsanyi@stud.uni-corvinus.hu>
2020-10-08 14:34:04 +0200

    Initial commit

---
+++ UserMaintenance/week5/Entities/Portfolioitem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week5.Entities
+{
+    public class Portfolioitem
+    {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
---
+++ UserMaintenance/week5/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using week5.Entities;
+
+namespace week5
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<Portfolioitem> Portfolio = new List<Portfolioitem>();
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new Portfolioitem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new Portfolioitem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new Portfolioitem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+    }
+}
